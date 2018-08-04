bda5087f3dffa354ebe9536e96ddd10f7f231c28
Ron Dekker <redkenrok@gmail.com>
2018-08-04 12:58:16 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# hoast-convert
-Hoast module used to convert the content of files using a specified function.
+# Hoast-convert
+Convert the content of files using a specified function.
+
+> As the name suggest this is a [Hoast](https://github.com/hoast/hoast#readme) module.
+
+## Usage
+
+### Parameters
+
+* `engine` **{Function}**: function that processes the content. The parameters are the file content and metadata. The metadata is the globally set metadata overridden with file's frontmatter. Frontmatter is only used if the [frontmatter module](https://github.com/hoast/hoast-frontmatter#readme) is used. This can be an asynchronous.
+	* Required: `yes`
+* `extension` **{String}**: The new extension name if it needs to change.
+	* Required: `no`
+* `patterns` **{Array of strings}**: An array of string which gets used to match files using glob patterns. See [nanomatch](https://github.com/micromatch/nanomatch#readme) for more details on the patterns.
+	* Required: `no`
+
+### Example
+
+**CLI**
+
+Not compatible with the CLI tool as it requires a reference to a self written function.
+
+**Script**
+
+```javascript
+const Hoast = require('hoast');
+const read = Hoast.read,
+      convert = require('hoast-convert');
+const CleanCSS = require('clean-css');
+
+const cleanCSS = new CleanCSS();
+
+Hoast(__dirname)
+  .use(read())
+  .use(convert({
+    engine: function(content, metadata) {
+      return cleanCSS.minify(content);
+	},
+    extension: 'min.css',
+    patterns: [
+      '**/*.css'
+    ]
+  }))
+  .process();
+```
+
+> In the example the CSS files are minified using [CleanCSS](https://github.com/jakubpawlowicz/clean-css#readme) and the `min` is prepended to the extension.
+
+## License
+[ISC license](https://github.com/hoast/hoast-convert/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,68 @@
+// Node modules.
+const assert = require('assert');
+// If debug available require it.
+let debug; try { debug = require('debug')('hoast-convert'); } catch(error) { debug = function() {}; }
+// Dependency modules.
+const nanomatch = require('nanomatch');
+
+/**
+ * Validates the module options.
+ * @param {Object} options The options.
+ */
+const validateOptions = function(options) {
+	assert(typeof(options) === 'object', 'hoast-convert: options must be of type object.');
+	assert(typeof(options.engine) === 'funtion', 'hoast-convert: engine must be specified and of type function.');
+	if (options.extension) {
+		assert(typeof(options.extension) === 'string', 'hoast-convert: extension must be of type string.');
+	}
+	if (options.patterns) {
+		assert(Array.isArray(options.patterns)  && options.patterns.length, 'hoast-convert: patterns needs must be specified and an array of strings.');
+	}
+};
+
+/**
+ * Convert the content of files.
+ * @param {Object} options The module options.
+ */
+module.exports = function(options) {
+	debug(`Initializing module.`);
+	
+	validateOptions(options);
+	debug(`Validated options.`);
+	options = Object.assign({
+		patterns: [
+			'**'
+		]
+	}, options);
+	
+	return async function(hoast, files) {
+		debug(`Running module.`);
+		await Promise.all(
+			// Loop through files.
+			files.map(function(file) {
+				return new Promise(function(resolve) {
+					debug(`Processing file '${file.path}'.`);
+					
+					assert(file.content !== null, 'hoast-convert: No content found on file, read module needs to be called before this.');
+					// Has to be a string and match patterns.
+					if (file.content.type !== 'string' || (options.patterns && options.patterns.length > 0 && !nanomatch.any(file.path, options.patterns))) {
+						debug(`File not valid for processing.`);
+						return resolve();
+					}
+					debug(`File content is valid.`);
+					
+					// Overwrite content with converted content.
+					file.content = await options.engine(file.content.data, { global: hoast.options.metadata, file: file.frontmatter });
+					debug(`File content converted.`);
+					
+					// Replace file extension.
+					if (options.extension) {
+						file.path = file.path.substr(0, file.path.lastIndexOf('.')).concat(options.extension);
+						debug(`Replacing file extension to '${file.path}'.`);
+					}
+					resolve();
+				})
+			})
+		);
+	};
+};
